502a953581a79b6b2d714460ca62a4635a7529ae
twr143 <twr143@gmail.com>
2020-03-23 10:45:34 +0300

    protobuffered Person-address, multiple versions, 1

--- src/main/scala/integration/kafka/serOn/protobuf/app/ConsApp.scala
+++ src/main/scala/integration/kafka/serOn/protobuf/app/ConsApp.scala
@@ -1,19 +1,25 @@
 package integration.kafka.serOn.protobuf.app
 import cats.effect.{ExitCode, IO, IOApp}
+import com.google.protobuf.CodedInputStream
 import com.typesafe.scalalogging.StrictLogging
 import fs2.kafka._
 import protomodels.person.Person
+import protomodels.person2.Person2
 import scala.concurrent.duration._
+import com.google.protobuf.any.Any
 
 /**
   * Created by Ilya Volynin on 22.03.2020 at 19:38.
   */
 object ConsApp extends IOApp with StrictLogging {
+  import protomodels.person.Person._
+  import protomodels.person2.Person2._
   val valueS = Deserializer.instance[IO, scalapb.GeneratedMessage] { (topic, headers, array) =>
-    headers("kkk")
     IO.delay {
-      println(s"topic = ${topic}, headers = $headers")
-      Person.parseFrom(array)
+      val any = Any.parseFrom(array)
+      if (any.is[Person]) any.unpack[Person]
+      else if (any.is[Person2]) any.unpack[Person2]
+      else Person("unpack failed", -1)
     }
   }
 
@@ -29,7 +35,7 @@ object ConsApp extends IOApp with StrictLogging {
     val s =
       consumerStream[IO]
         .using(consumerSettings)
-        .evalTap(_.subscribeTo("22marProtoPerson1"))
+        .evalTap(_.subscribeTo("23marProto2"))
         .flatMap(_.stream)
         .mapAsync(25) { committable =>
           processRecord(committable.record).map(_ => committable.offset)
--- src/main/scala/integration/kafka/serOn/protobuf/app/ProdApp.scala
+++ src/main/scala/integration/kafka/serOn/protobuf/app/ProdApp.scala
@@ -2,10 +2,14 @@ package integration.kafka.serOn.protobuf.app
 import java.time.LocalDate
 
 import cats.effect.{ExitCode, IO, IOApp}
+import com.google.protobuf.any.Any
+import scalapb.GeneratedMessage
 import fs2.Stream
 import fs2.kafka._
 import protomodels.person.Person
 import protomodels.person.Person.PhoneNumber
+import protomodels.person2.Person2.PhoneNumber2
+import protomodels.person2.Person2
 
 /**
   * Created by Ilya Volynin on 22.03.2020 at 19:10.
@@ -15,22 +19,25 @@ object ProdApp extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
     val p = Person("Ilya", 123, None, List(PhoneNumber("7900000001"), PhoneNumber("7900000002")))
     //    val tryDecode: scala.util.Try[Any] = KryoInjection.invert(bytes)
-    val valueS = Serializer.instance[IO, Person] { (topic, headers, p) =>
-      IO.pure(p.toByteArray)
+    val valueS = Serializer.instance[IO, GeneratedMessage] { (topic, headers, p) =>
+      { IO.pure(Any.pack(p).toByteArray) }
     }
-    val producerSettings = ProducerSettings[IO, String, Person](keySerializer = Serializer[IO, String], valueSerializer = valueS)
+    val producerSettings = ProducerSettings[IO, String, GeneratedMessage](keySerializer = Serializer[IO, String], valueSerializer = valueS)
       .withBootstrapServers("localhost:9093")
-    val s = Stream(4, 5, 6)
-      .map(i => Person(s"Ilya$i", 123 * i, None, List(PhoneNumber("7900000001"), PhoneNumber("7900000002"))))
-      .evalMap(p =>
-        IO(
-          ProducerRecords.one(
-            ProducerRecord("22marProtoPerson1", p.name, p)
-              .withHeaders(Headers(Header("kkk", "vvv")))
-          )
-        )
+    val s = Stream(4, 5, 6, 7)
+      .map(i =>
+        if (i % 2 == 0)
+          Person(s"Ilya$i", 123 * i, None, List(PhoneNumber("7900000001"), PhoneNumber("7900000002")))
+        else
+          Person2(s"2Ilya$i", 123 * i, Some(s"$i@mail.com"), List(PhoneNumber2("7900000001"), PhoneNumber2("7900000002")))
       )
-      .through(produce[IO, String, Person, Unit](producerSettings))
+      .evalMap {
+        case p: Person =>
+          IO(ProducerRecords.one(ProducerRecord("23marProto2", p.name, p).withHeaders(Headers(Header("p", "vvv")))))
+        case p: Person2 =>
+          IO(ProducerRecords.one(ProducerRecord("23marProto2", p.name, p).withHeaders(Headers(Header("p2", "vvv")))))
+      }
+      .through(produce[IO, String, GeneratedMessage, Unit](producerSettings))
     s.compile.drain.map(_ => ExitCode.Success)
   }
 }
