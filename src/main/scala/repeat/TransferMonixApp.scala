262ce58763ff2cdfed3f941b58d9218570b2edd8
twr143 <twr143@gmail.com>
2020-03-28 12:43:52 +0300

    transfer monix, refactoring, 1

--- src/main/scala/repeat/TransferMonixApp.scala
+++ src/main/scala/repeat/TransferMonixApp.scala
@@ -1,22 +1,22 @@
 package repeat
 import cats.effect.{ContextShift, ExitCode, Timer}
 import monix.eval.Task
+import monix.execution.Scheduler
 import repeat.RepeatCommon.{Bank, Counter}
-import repeat.RepeatMonixApp.{firstTask, secondTask}
-import monix.execution.Scheduler.Implicits.global
 
 /**
- * Created by Ilya Volynin on 19.03.2020 at 11:07.
- */
+  * Created by Ilya Volynin on 19.03.2020 at 11:07.
+  */
 object TransferMonixApp extends App with RepeatCommon {
-  implicit val cs: ContextShift[Task] = Task.contextShift(global)
-
-  implicit val timer: Timer[Task] = Task.timer(global)
+  implicit val sche = Scheduler.forkJoin(5, 10, "transfer-scheduler")
+  implicit val timer: Timer[Task] = Task.timer(sche)
   implicit val effect = Task.catsEffect
-  Task.parZip3(firstTransfer[Task](Bank.transfer),secondTransfer[Task](Bank.transfer),
-    thirdTransfer[Task](Bank.transfer) ).map(_ => {
-        println("all transfers completed...");
-        println(s"accA...${Bank.accA}, accB... ${Bank.accB}");
-        ExitCode.Success
-      }).runSyncUnsafe()
+  Task
+    .parZip3(firstTransfer[Task](Bank.transfer), secondTransfer[Task](Bank.transfer), thirdTransfer[Task](Bank.transfer))
+    .map(_ => {
+      println("all transfers completed...");
+      println(s"accA...${Bank.accA}, accB... ${Bank.accB}");
+      ExitCode.Success
+    })
+    .runSyncUnsafe()
 }
