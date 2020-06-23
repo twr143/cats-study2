2e766b0c85981c4a599f9f3a48ee9fb3333f323f
twr143 <twr143@gmail.com>
2020-06-23 16:02:06 +0300

    resource usage, fibers, 2

--- src/main/scala/cats/resource/ResFiberE.scala
+++ src/main/scala/cats/resource/ResFiberE.scala
@@ -1,47 +1,58 @@
 package cats.resource
-import cats.effect._
+import cats.effect.Resource
 import com.typesafe.scalalogging.StrictLogging
 import monix.eval.{Fiber, Task}
 import monix.execution.Scheduler.Implicits.global
 
 import scala.concurrent.duration._
+import cats.implicits._
 
 /**
   * Created by Ilya Volynin on 22.06.2020 at 12:24.
   */
 object ResFiberE extends StrictLogging {
+
   def main(args: Array[String]): Unit = {
-
-    implicit val cs: ContextShift[Task] = Task.contextShift(global)
-
-    implicit val timer: Timer[Task] = Task.timer(global)
-    implicit val effect = Task.catsEffect
     case class A(id: Long = 0)
-//
-    def res =
-      Resource.make((Task { logger.info("aquire"); 0 } /*>> Task.raiseError[Int](new Exception("BOOM!"))*/ ).start)(_ =>
-        Task { logger.info("relelase begun") } >> Task.sleep(500 millis) >> Task { logger.info("relelase completed") }
+    //
+    lazy val res =
+      Resource.make((Task {
+        logger.info("aquire"); 0
+      } >> Task.raiseError[Int](new Exception("Res BOOM!"))).start)(_ =>
+        Task {
+          logger.info("relelase begun")
+        } >> Task.sleep(500 millis) >> Task {
+          logger.info("relelase completed")
+        }
       )
-    def res2 =
-      Resource.make(Task { logger.info("aquire 2"); 1 }.start /*>> Task.raiseError[Unit](new Exception("BOOM 2!")).start*/ )(_ =>
-        Task { logger.info("relelase 2 begun") } >> Task.sleep(500 millis) >> Task { logger.info("relelase 2 completed") }
+    lazy val res2 =
+      Resource.make(Task {
+        logger.info("aquire 2"); 1
+      }.start /*>> Task.raiseError[Unit](new Exception("BOOM 2!")).start*/ )(_ =>
+        Task {
+          logger.info("relelase 2 begun")
+        } >> Task.sleep(500 millis) >> Task {
+          logger.info("relelase 2 completed")
+        }
       )
     (for {
       r <- res
       r2 <- res2
     } yield (r, r2))
       .use {
-
-//  (res, res2).mapN((_, _)).use {
+        //  (res, res2).mapN((_, _)).use {
         case (r, r2) =>
-          (for {
+          for {
             j <- r.join
             j2 <- r2.join
-          } yield (j, j2)).flatMap {
-            case (j, j2) =>
-              Task { logger.info("res use {} {}", j, j2) } >> Task.raiseError[Unit](new Exception("BOOM 2!")) >> Task { logger.info("done") }
-          }
+            _ <- Task {
+              logger.info("res use {} {}", j, j2)
+            }
+            _ <- Task.raiseError[Unit](new Exception("BOOM 2!"))
+            _ <- Task {
+              logger.info("done")
+            }
+          } yield ()
       } runSyncUnsafe ()
-
   }
 }
