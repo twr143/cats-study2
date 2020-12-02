7296be1ef4e50c7eff146d00231e83fde5add049
twr143 <twr143@gmail.com>
2020-12-02 12:41:22 +0300

    fs2 grpc client, 1

---
+++ src/main/scala/integration/grpc/Client.scala
@@ -0,0 +1,36 @@
+package integration.grpc
+import cats.effect.{ExitCode, IO, IOApp}
+import grpc.model.hello.{GreeterFs2Grpc, HelloRequest}
+import io.grpc._
+import fs2._
+import org.lyranthe.fs2_grpc.java_runtime.implicits._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+  * Created by Ilya Volynin on 02.12.2020 at 12:15.
+  */
+object Client extends IOApp {
+  val managedChannelStream: Stream[IO, ManagedChannel] =
+    ManagedChannelBuilder
+      .forAddress("127.0.0.1", 9999)
+      .usePlaintext()
+      .stream[IO]
+
+  def runProgram(helloStub: GreeterFs2Grpc[IO, Metadata]): IO[Unit] = {
+    for {
+      response <- helloStub.sayHello(HelloRequest("Ilyusha V"), new Metadata())
+      _ <- IO(println(response.greeting))
+    } yield ()
+  }
+
+  def run(args: scala.List[String]): cats.effect.IO[cats.effect.ExitCode] = {
+    (for {
+      managedChannel <- managedChannelStream
+      helloStub = GreeterFs2Grpc.stub[IO](managedChannel)
+      _ <- Stream.eval(runProgram(helloStub))
+    } yield ExitCode.Success).compile.drain
+      .map(_ => ExitCode.Success)
+
+  }
+}
